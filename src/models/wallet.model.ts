cc8ededbca6ff9a8672a4608a7ba4525396817f5
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2022-07-18 16:05:41 +0530

    Wallet Model code

---
+++ src/models/wallet.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Wallet extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  walletId?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  balance: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  userId: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  createdAt: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  updatedAt: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Wallet>) {
+    super(data);
+  }
+}
+
+export interface WalletRelations {
+  // describe navigational properties here
+}
+
+export type WalletWithRelations = Wallet & WalletRelations;

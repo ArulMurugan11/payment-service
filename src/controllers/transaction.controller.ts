b2c5a22b33412620ccec62570896e9b4fb585713
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2022-08-12 18:47:36 +0530

    Implemented the code for download statement in transaction API and added the html template for the statement

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -31,11 +31,17 @@ import {API_PREFIX, LoggingBindings, MONTHS} from '../key';
 import {Transaction} from '../models';
 import {
   TransactionRepository,
+  TransferRepository,
   WalletAuditRepository,
   WalletRepository,
 } from '../repositories';
 import {IndulgeRestService} from '../services/indulge.service';
 const qs = require('qs');
+const easyinvoice = require('easyinvoice');
+const fs = require('fs');
+//const btoa = require('btoa');
+//const ejs = require('ejs');
+//const pdf = require('html-pdf');
 
 export class TransactionController {
   constructor(
@@ -49,6 +55,8 @@ export class TransactionController {
     private logger: WinstonLogger,
     @inject(RestBindings.Http.RESPONSE)
     public res: Response,
+    @repository(TransferRepository)
+    public transferRepository: TransferRepository,
     @inject(RestBindings.Http.REQUEST)
     public request: Request,
     @inject('services.IndulgeService')
@@ -215,6 +223,153 @@ export class TransactionController {
     });
 
     return groupedTransactions;
+  }
+
+  @logInvocation()
+  @get(`${API_PREFIX}/transactions/downloadtxn`)
+  @response(200, {
+    description: 'Array of Transaction model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'string',
+        },
+      },
+    },
+  })
+  async downloadTxn(
+    //@param.path.string('role') role: string,
+    @param.filter(Transaction) filter?: Filter<Transaction>,
+  ): Promise<any> {
+    const user = this.res?.locals?.user;
+    const userRole = user.roles.name;
+    console.log('///////////////////////////////');
+    console.log(user);
+    console.log(__dirname);
+    console.log('//////////////////////////');
+    // if (userRole == 'customer') {
+    const userWalletAudit = await this.WalletAuditRepository.find({
+      where: {
+        userId: user.userId,
+      },
+    });
+    // Our new data object, this will replace the empty object we used earlier.
+    var data = {
+      // Let's add a recipient
+      client: {
+        company: 'Client Corp',
+        address: 'Clientstreet 456',
+        zip: '4567 CD',
+        city: 'Clientcity',
+        country: 'Clientcountry',
+      },
+
+      // Now let's add our own sender details
+      sender: {
+        company: 'Sample Corp',
+        address: 'Sample Street 123',
+        zip: '1234 AB',
+        city: 'Sampletown',
+        country: 'Samplecountry',
+      },
+
+      // Of course we would like to use our own logo and/or background on this invoice. There are a few ways to do this.
+      images: {
+        //      Logo:
+        // 1.   Use a url
+        logo: 'https://public.easyinvoice.cloud/img/logo_en_original.png',
+        /*
+         2.   Read from a local file as base64
+              logo: fs.readFileSync('logo.png', 'base64'),
+         3.   Use a base64 encoded image string*/
+      },
+
+      // Let's add some standard invoice data, like invoice number, date and due-date
+      information: {
+        // Invoice number
+        number: '2021.0001',
+        // Invoice data
+        date: '12-12-2021',
+        // Invoice due date
+        'due-date': '31-12-2021',
+      },
+
+      // Now let's add some products! Calculations will be done automatically for you.
+      products: [
+        {
+          quantity: '2',
+          description: 'Test1',
+          'tax-rate': 6,
+          price: 33.87,
+        },
+        {
+          quantity: '4',
+          description: 'Test2',
+          'tax-rate': 21,
+          price: 10.45,
+        },
+      ],
+
+      // We will use bottomNotice to add a message of choice to the bottom of our invoice
+      bottomNotice: 'Kindly pay your invoice within 15 days.',
+
+      // Here you can customize your invoice dimensions, currency, tax notation, and number formatting based on your locale
+      settings: {
+        currency: 'INR', // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
+        /*
+       "locale": "nl-NL", // Defaults to en-US, used for number formatting (See documentation 'Locales and Currency')
+       "tax-notation": "gst", // Defaults to 'vat'
+       // Using margin we can regulate how much white space we would like to have from the edges of our invoice
+       "margin-top": 25, // Defaults to '25'
+       "margin-right": 25, // Defaults to '25'
+       "margin-left": 25, // Defaults to '25'
+       "margin-bottom": 25, // Defaults to '25'
+       "format": "A4", // Defaults to A4, options: A3, A4, A5, Legal, Letter, Tabloid
+       "height": "1000px", // allowed units: mm, cm, in, px
+       "width": "500px", // allowed units: mm, cm, in, px
+       "orientation": "landscape", // portrait or landscape, defaults to portrait
+       */
+      },
+
+      /*
+      Last but not least, the translate parameter.
+      Used for translating the invoice to your preferred language.
+      Defaults to English. Below example is translated to Dutch.
+      We will not use translate in this sample to keep our samples readable.
+   */
+      translate: {
+        /*
+       "invoice": "FACTUUR",  // Default to 'INVOICE'
+       "number": "Nummer", // Defaults to 'Number'
+       "date": "Datum", // Default to 'Date'
+       "due-date": "Verloopdatum", // Defaults to 'Due Date'
+       "subtotal": "Subtotaal", // Defaults to 'Subtotal'
+       "products": "Producten", // Defaults to 'Products'
+       "quantity": "Aantal", // Default to 'Quantity'
+       "price": "Prijs", // Defaults to 'Price'
+       "product-total": "Totaal", // Defaults to 'Total'
+       "total": "Totaal" // Defaults to 'Total'
+       */
+      },
+
+      /*
+      Customize enables you to provide your own templates.
+      Please review the documentation for instructions and examples.
+      Leave this option blank to use the default template
+   */
+      customize: {
+        template: fs.readFileSync('template.html', 'base64'), // Must be base64 encoded html
+      },
+    };
+    easyinvoice.createInvoice(data, function (result: any) {
+      /*
+          5.  The 'result' variable will contain our invoice as a base64 encoded PDF
+              Now let's save our invoice to our local filesystem so we can have a look!
+              We will be using the 'fs' library we imported above for this.
+      */
+      fs.writeFileSync('Statement.pdf', result.pdf, 'base64');
+    });
+    return userWalletAudit;
   }
 
   @logInvocation()
